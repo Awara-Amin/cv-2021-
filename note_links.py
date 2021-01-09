683fc0fe27c516ff61498a43579dfcb1342caea6
Awara. Amin <67450869+Awara-Amin@users.noreply.github.com>
2021-01-09 15:08:46 +0300

    Initial CV website files

---
+++ note_links.py
@@ -0,0 +1,14 @@
+
+video 38 >>> CSS rules
+changing color
+
+h1{color:red;} >> it means >> who{what:how;}
+
+
+
+
+
+
+
+if my love of becan affends you, i am depply appologwtic
+whatevever you worship, is meat, vigetable
